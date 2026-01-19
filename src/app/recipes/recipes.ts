173e3f0f765653a8ea1ecc37b2127cd30bdccfe1
amgithshaji <amgithshaji410@gmail.com>
2026-01-20 00:52:55 +0530

    register login

--- src/app/recipes/recipes.ts
+++ src/app/recipes/recipes.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject,signal } from '@angular/core';
 import { Footer } from '../footer/footer';
 import { Header } from '../header/header';
+import { ApiService } from '../services/api-service';
 
 @Component({
   selector: 'app-recipes',
@@ -9,5 +10,24 @@ import { Header } from '../header/header';
   styleUrl: './recipes.css',
 })
 export class Recipes {
+
+
+  allRecipes:any = signal([])
+
+  api = inject(ApiService)
+    ngOnInit(){
+    this.getAllRecipes()
+  }
+
+  getAllRecipes(){
+   this.api.getAllRecipes().subscribe((res:any)=>{
+    console.log(res);
+    this.allRecipes.set(res)
+    console.log(this.allRecipes());
+    
+    
+    
+   })
+  }
 
 }
